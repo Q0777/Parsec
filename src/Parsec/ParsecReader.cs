586dafbff62d02c5d58a0426d60a6eb102dc0e53
Matias Ramirez <matigramirez@gmail.com>
2026-03-10 21:09:39 -0300

    fix(reader): remove 'async' methods

--- src/Parsec/ParsecReader.cs
+++ src/Parsec/ParsecReader.cs
@@ -26,18 +26,6 @@ public static class ParsecReader
     }
 
     /// <summary>
-    /// Reads a shaiya file format from a file
-    /// </summary>
-    /// <param name="path">File path</param>
-    /// <param name="episode">File episode</param>
-    /// <param name="encoding">File encoding</param>
-    /// <typeparam name="T">Shaiya File Format Type</typeparam>
-    public static Task<T> FromFileAsync<T>(string path, Episode episode = Episode.EP5, Encoding? encoding = null) where T : FileBase, new()
-    {
-        return Task.FromResult(FromFile<T>(path, episode, encoding));
-    }
-
-    /// <summary>
     /// Reads the shaiya file format from a file
     /// </summary>
     /// <param name="path">File path</param>
@@ -49,18 +37,6 @@ public static class ParsecReader
     {
         var serializationOptions = new BinarySerializationOptions(episode, encoding);
         return FileBase.ReadFromFile(path, type, serializationOptions);
-    }
-
-    /// <summary>
-    /// Reads the shaiya file format from a file
-    /// </summary>
-    /// <param name="path">File path</param>
-    /// <param name="type">FileBase child type to be read</param>
-    /// <param name="episode">File episode</param>
-    /// <param name="encoding">File encoding</param>
-    public static Task<FileBase> FromFileAsync(string path, Type type, Episode episode = Episode.EP5, Encoding? encoding = null)
-    {
-        return Task.FromResult(FromFile(path, type, episode, encoding));
     }
 
     /// <summary>
@@ -79,19 +55,6 @@ public static class ParsecReader
     }
 
     /// <summary>
-    /// Reads a shaiya file format from a buffer (byte array)
-    /// </summary>
-    /// <param name="name">File name</param>
-    /// <param name="buffer">File buffer</param>
-    /// <param name="episode">File episode</param>
-    /// <param name="encoding">File encoding</param>
-    /// <typeparam name="T">Shaiya File Format Type</typeparam>
-    public static Task<T> FromBufferAsync<T>(string name, byte[] buffer, Episode episode = Episode.EP5, Encoding? encoding = null) where T : FileBase, new()
-    {
-        return Task.FromResult(FromBuffer<T>(name, buffer, episode));
-    }
-
-    /// <summary>
     /// Reads the shaiya file format from a buffer (byte array)
     /// </summary>
     /// <param name="name">File name</param>
@@ -107,19 +70,6 @@ public static class ParsecReader
     }
 
     /// <summary>
-    /// Reads the shaiya file format from a buffer (byte array)
-    /// </summary>
-    /// <param name="name">File name</param>
-    /// <param name="buffer">File buffer</param>
-    /// <param name="type">FileBase child type to be read</param>
-    /// <param name="episode">File episode</param>
-    /// <param name="encoding">File encoding</param>
-    public static Task<FileBase> FromBufferAsync(string name, byte[] buffer, Type type, Episode episode = Episode.EP5, Encoding? encoding = null)
-    {
-        return Task.FromResult(FromBuffer(name, buffer, type, episode, encoding));
-    }
-
-    /// <summary>
     /// Reads a shaiya file format from a json file
     /// </summary>
     /// <param name="path">Path to json file</param>
@@ -129,17 +79,6 @@ public static class ParsecReader
     public static T FromJsonFile<T>(string path, Encoding? encoding = null) where T : FileBase
     {
         return (T)FromJsonFile(path, typeof(T), encoding);
-    }
-
-    /// <summary>
-    /// Reads a shaiya file format from a json file
-    /// </summary>
-    /// <param name="path">Path to json file</param>
-    /// <param name="encoding">String encoding</param>
-    /// <typeparam name="T"><see cref="FileBase"/> type</typeparam>
-    public static Task<T> FromJsonFileAsync<T>(string path, Encoding? encoding = null) where T : FileBase
-    {
-        return Task.FromResult(FromJsonFile<T>(path, encoding));
     }
 
     /// <summary>
@@ -186,17 +125,6 @@ public static class ParsecReader
     /// <summary>
     /// Reads a shaiya file format from a json file
     /// </summary>
-    /// <param name="path">Path to json file</param>
-    /// <param name="type">FileBase child type to be read</param>
-    /// <param name="encoding">String encoding</param>
-    public static Task<FileBase> FromJsonFileAsync(string path, Type type, Encoding? encoding = null)
-    {
-        return Task.FromResult(FromJsonFile(path, type, encoding));
-    }
-
-    /// <summary>
-    /// Reads a shaiya file format from a json file
-    /// </summary>
     /// <param name="name">Instance name</param>
     /// <param name="jsonText">json text</param>
     /// <param name="encoding">String encoding</param>
@@ -205,18 +133,6 @@ public static class ParsecReader
     public static T FromJson<T>(string name, string jsonText, Encoding? encoding = null) where T : FileBase
     {
         return (T)FromJson(name, jsonText, typeof(T), encoding);
-    }
-
-    /// <summary>
-    /// Reads a shaiya file format from a json file
-    /// </summary>
-    /// <param name="name">Instance name</param>
-    /// <param name="jsonText">json text</param>
-    /// <param name="encoding">String encoding</param>
-    /// <typeparam name="T"><see cref="FileBase"/> type</typeparam>
-    public static Task<T> FromJsonAsync<T>(string name, string jsonText, Encoding? encoding = null) where T : FileBase
-    {
-        return Task.FromResult(FromJson<T>(name, jsonText, encoding));
     }
 
     /// <summary>
@@ -248,18 +164,6 @@ public static class ParsecReader
     }
 
     /// <summary>
-    /// Reads a shaiya file format from a json file
-    /// </summary>
-    /// <param name="name">Instance name</param>
-    /// <param name="jsonText">json text</param>
-    /// <param name="type">FileBase child type to be read</param>
-    /// <param name="encoding">String encoding</param>
-    public static Task<FileBase> FromJsonAsync(string name, string jsonText, Type type, Encoding? encoding = null)
-    {
-        return Task.FromResult(FromJson(name, jsonText, type, encoding));
-    }
-
-    /// <summary>
     /// Reads the shaiya file format from a buffer (byte array) within a <see cref="Data"/> instance
     /// </summary>
     /// <param name="data"><see cref="Data"/> instance</param>
@@ -278,18 +182,6 @@ public static class ParsecReader
     /// </summary>
     /// <param name="data"><see cref="Data"/> instance</param>
     /// <param name="file"><see cref="SFile"/> instance</param>
-    /// <param name="episode">File episode</param>
-    /// <param name="encoding">File encoding</param>
-    public static Task<T> FromDataAsync<T>(Data data, SFile file, Episode episode = Episode.EP5, Encoding? encoding = null) where T : FileBase, new()
-    {
-        return Task.FromResult(FromData<T>(data, file, episode, encoding));
-    }
-
-    /// <summary>
-    /// Reads the shaiya file format from a buffer (byte array) within a <see cref="Data"/> instance
-    /// </summary>
-    /// <param name="data"><see cref="Data"/> instance</param>
-    /// <param name="file"><see cref="SFile"/> instance</param>
     /// <param name="type">FileBase child type to be read</param>
     /// <param name="episode">File episode</param>
     /// <param name="encoding">File encoding</param>
@@ -298,18 +190,5 @@ public static class ParsecReader
     {
         var serializationOptions = new BinarySerializationOptions(episode, encoding);
         return FileBase.ReadFromData(data, file, type, serializationOptions);
-    }
-
-    /// <summary>
-    /// Reads the shaiya file format from a buffer (byte array) within a <see cref="Data"/> instance
-    /// </summary>
-    /// <param name="data"><see cref="Data"/> instance</param>
-    /// <param name="file"><see cref="SFile"/> instance</param>
-    /// <param name="type">FileBase child type to be read</param>
-    /// <param name="episode">File episode</param>
-    /// <param name="encoding">File encoding</param>
-    public static Task<FileBase> FromDataAsync(Data data, SFile file, Type type, Episode episode = Episode.EP5, Encoding? encoding = null)
-    {
-        return Task.FromResult(FromData(data, file, type, episode, encoding));
     }
 }
